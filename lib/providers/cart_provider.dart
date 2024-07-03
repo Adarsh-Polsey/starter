cc9cbaa58bd0b1bf49dee3f112dce5dc27dc7a7b
Adarsh Polsey <69569984+Adarsh-Polsey@users.noreply.github.com>
2024-07-03 11:36:50 +0530

    test notifier

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,19 +1,22 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
+part 'cart_provider.g.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+class CartNotifier extends _$CartNotifier {
   @override
   Set<Product> build() {
-    return const {
-      Product(
-          id: '1',
-          title: 'Groovy Shorts',
-          price: 12.99,
-          image: 'assets/products/shorts.png')
-    };
+    return const {};
+  }
+
+  void addProduct(Product product) {
+    if (!state.contains(product)) state = {...state, product};
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) {
+        return p.id != product.id;
+      }).toSet();
+    }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
